d2be018e62886f9c5208ae6c61d68e9fdc678ade
Vladislav <vladgm233223@gmail.com>
2022-12-14 19:44:53 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import MainLayout from "./components/layout/MainLayout";
+import NotFound from "./components/NotFound";
 
 function App() {
-  return <div className="App">Hello World!!!</div>;
+  return (
+    <section className="wrapper">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+
+          <Route path="contacts" element={<About />} /> 
+        
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </section>
+  );
 }
 
 export default App;
