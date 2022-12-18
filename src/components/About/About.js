52fffd2fb00f9f112cd1ad6c78f7746a2cd506d4
Vladislav <vladgm233223@gmail.com>
2022-12-18 15:18:26 +0200

    add about page

---
+++ src/components/About/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./About.css";
+
+
+
+const About = () => {
+
+
+  
+
+  return (
+    <section>
+      <h2 className="aboutH">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+      <p className="aboutP">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Bibendum at varius
+        vel pharetra vel turpis nunc eget. Eget gravida cum sociis natoque
+        penatibus. Purus faucibus ornare suspendisse sed nisi lacus sed viverra.
+        Ut sem viverra aliquet eget sit amet tellus. Rutrum tellus pellentesque
+        eu tincidunt. Id semper risus in hendrerit gravida rutrum.
+      </p>
+      <h2 className="aboutH">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+      <p className="aboutP">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Bibendum at varius
+        vel pharetra vel turpis nunc eget. Eget gravida cum sociis natoque
+        penatibus. Purus faucibus ornare suspendisse sed nisi lacus sed viverra.
+        Ut sem viverra aliquet eget sit amet tellus. Rutrum tellus pellentesque
+        eu tincidunt. Id semper risus in hendrerit gravida rutrum.
+      </p>
+      <h2 className="aboutH">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h2>
+      <p className="aboutP">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Bibendum at varius
+        vel pharetra vel turpis nunc eget. Eget gravida cum sociis natoque
+        penatibus. Purus faucibus ornare suspendisse sed nisi lacus sed viverra.
+        Ut sem viverra aliquet eget sit amet tellus. Rutrum tellus pellentesque
+        eu tincidunt. Id semper risus in hendrerit gravida rutrum.
+      </p>
+    </section>
+  );
+};
+
+export default About;
