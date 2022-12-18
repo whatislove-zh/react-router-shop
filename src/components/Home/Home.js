c579ecf497a202902b1bd7d1d76555dc0eb98863
Vladislav <vladgm233223@gmail.com>
2022-12-18 15:17:54 +0200

    Create Home.js

---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import Slider from "./Slider";
+
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <>
+      <Slider>
+        <section className="firstSlide">
+          <h1 className="slideText">Slide Text1</h1>
+        </section>
+        <section className="secondSlide">
+          <h1 className="slideText">Slide Text2</h1>
+        </section>
+        <section className="thirdSlide">
+          <h1 className="slideText">Slide Text13</h1>
+        </section>
+      </Slider>
+    </>
+  );
+};
+
+export default Home;
