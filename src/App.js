52b8936bebe491e1f6a48ad3096d9f901e4b9175
Vladislav <vladgm233223@gmail.com>
2022-12-20 17:04:27 +0200

    function for adding to cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About/About";
 import Catalog from "./components/Catalog/Catalog";
@@ -6,22 +7,47 @@ import Contacts from "./components/Contacts/Contacts";
 import Home from "./components/Home/Home";
 import MainLayout from "./components/layout/MainLayout";
 import NotFound from "./components/NotFound";
+import ShopingCart from "./components/ShopingCart/ShopingCart";
 
 function App() {
+  const [shopingCartProducts, setShopingCartProducts] = useState([]);
+
+  const addToCart = (product) => {
+    let flag = true;
+
+    shopingCartProducts.some((el) => {
+      if (JSON.stringify(el) === JSON.stringify(product)) {
+        flag = false;
+        return false;
+      }
+
+      return false;
+    });
+
+    flag && setShopingCartProducts((prev) => [...prev, product]);
+  };
+
   return (
     <section className="wrapper">
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
 
-          <Route path="catalog" element={<Catalog />} />
-
-          <Route path="catalog/:slug" element={<ProductsItem />} />
+          <Route path="catalog" element={<Catalog addToCart={addToCart} />} />
+          <Route
+            path="catalog/:slug"
+            element={<ProductsItem addToCart={addToCart} />}
+          />
 
           <Route path="about" element={<About />} />
 
-          <Route path="contacts" element={<Contacts />} /> 
-        
+          <Route path="contacts" element={<Contacts />} />
+
+          <Route
+            path="shoping-cart"
+            element={<ShopingCart shopingCartProducts={shopingCartProducts} />}
+          />
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
